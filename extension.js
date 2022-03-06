06acba34f4e76490fc7ab086a028c3f94ee849cb
Dave <DJSaunders1997@gmail.com>
2022-03-06 10:37:43 +0000

    Adding comments

--- extension.js
+++ extension.js
@@ -11,6 +11,9 @@ function activate(context) {
 
 	console.log('Congratulations, your extension "eggtension" is now active!');
 
+	// Build Conda Env Command
+	// This command will build a conda environment from the file active in window.
+	// It first gets the name of the active file, removes escape character \\ and then runs "conda env create -f file"
 	let buildCondaYAMLFunct = vscode.commands.registerCommand('eggtension.buildCondaYAML',
 		function () {
 
@@ -20,6 +23,8 @@ function activate(context) {
 			// https://stackoverflow.com/questions/53076566/visual-studio-code-extension-getting-active-tab-data-for-non-textual-files
 			const activeEditor = vscode.window.activeTextEditor;
 
+			// TODO: Validation that file ends in .yaml or .yml
+			// If not exit function and show vscode information message saying "Cannot build conda env from a {fileExtension} file. Only YAML files are supported."
 			var filename = activeEditor.document.fileName
 
 			console.log(`Filename is :${filename}`);
@@ -41,9 +46,16 @@ function activate(context) {
 	);
 	context.subscriptions.push(buildCondaYAMLFunct);
 
+
+
+
+	// Delete Conda Env Command
+	// This command will delete a conda environment given the name of the environment from the user.
 	let deleteCondaEnvFunct = vscode.commands.registerCommand('eggtension.deleteCondaEnv',
 	function () {
 
+		// TODO: Can I get the same list of python interpreters as used by the python extension?
+		//	Then I could filter them down to only conda envs and will be more robust and prevent me from reinventing the wheel.
 		// TODO list conda envs to use as dropdown / selection window
 		// Run conda env list and parse results into a useable object / dict
 
