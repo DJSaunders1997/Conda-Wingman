913f2cd9999d9db9fb317b552f69fbaf215765e7
David Saunders <Djsaunders1997@gmail.com>
2022-03-06 13:06:10 +0000

    Base working functionality (#1)
    * First commit of initial changes from default extension
    
    * Update readme to include sources
    
    * Adding example conda yml file for testing
    
    * Replacing default readme with TODO
    
    * Adding breif description to readme
    
    * Capitalise E
    
    * Adding buildCondaYAML non-functional code
    
    * First working version to create conda envs from file
    
    * Remove example commands
    
    * Formatting code
    
    * Initial delete env code
    
    * Remove old functions
    
    * Adding todos
    
    * Adding comments
    
    * Initial show input box test
    
    * Added delete environment functionality
    
    * Formatting document
    
    * adding TODOs
    
    * Spelling fixes

--- README.md
+++ README.md
@@ -1,65 +1,35 @@
-# eggtension README
+# Eggtension README
 
-This is the README for your extension "eggtension". After writing up a brief description, we recommend including the following sections.
+This is the README for the WIP extension "Eggtension". 
+This extension helps to "crack the code" of working with python environments using conda.
+Most features are QoL improvements that help programmers use environments without having to memorise all of the conda commands.
 
-## Features
+# TODO 
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+## TODO Features:
+- Create conda environment from running command when conda environment is open
+    - Functionality should be easy to implement with current codebase.
+- Create conda environment from running command when conda environment is NOT open. 
+    - (Scan all .yml files in working directory and then build any that look like valid yml)
+    - (If there are more than 1 yml file display a dropdown to the user so they can choose which yml to build off of)
+- Auto-read filename each time a new file is opened. Then add button/tooltip/inline message asking users if they want to build this conda file.
+- Detect when new packages are added to the conda env and ask user if they want to add to the yaml.
+- If users try to build an environment that already exists then show prompt asking if they would like to overwrite existing environment.
+- Easy deletion of environments.
 
-For example if there is an image subfolder under your extension project workspace:
+## TODO Fixes
+- Commands should be OS agnostic.
+    - Failing this users should at least be able to enter their OS in a config file somewhere.
+    - Can we get the running OS in JavaScript and then have a conditional to run command for only that OS each time we want to run a command?
 
-\!\[feature X\]\(images/feature-x.png\)
+### Sources Used
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+This is a non-comprehensive list of resources I used to go from 0 javascript knowledge to creating an extension.
 
-## Requirements
+- https://code.visualstudio.com/api/get-started/your-first-extension
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+YouTube Vids:
+- Creating A Simple VSCode Extension https://www.youtube.com/watch?v=srwsnNhiqv8
+- How To Create And Deploy A VSCode Extension https://www.youtube.com/watch?v=q5V4T3o3CXE 
+- How to Code a VSCode Extension https://www.youtube.com/watch?v=a5DX5pQ9p5M
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- extension.js
+++ extension.js
@@ -1,36 +1,128 @@
+// This file contains the main logic for the extension
+
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// The module 'execSync' is used to execute shell commands: https://stackoverflow.com/questions/1880198/how-to-execute-shell-command-in-javascript
+
 const vscode = require('vscode');
+const execSync = require('child_process').execSync;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "eggtension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('eggtension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// Build Conda Env Command
+	// This command will build a conda environment from the file active in window.
+	// It first gets the name of the active file, removes escape character \\ and then runs "conda env create -f file"
+	let buildCondaYAMLFunct = vscode.commands.registerCommand('eggtension.buildCondaYAML',
+		function () {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Eggtension!');
-	});
+			// TODO Add text / button to status bar that says something like "Create Env from file". Maybe with a snazzy logo too
+			// similar to this https://marketplace.visualstudio.com/items?itemName=RoscoP.ActiveFileInStatusBar
 
-	context.subscriptions.push(disposable);
+			// https://stackoverflow.com/questions/53076566/visual-studio-code-extension-getting-active-tab-data-for-non-textual-files
+			const activeEditor = vscode.window.activeTextEditor;
+
+			// TODO: Validation that file ends in .yaml or .yml
+			// If not exit function and show vscode information message saying "Cannot build conda env from a {fileExtension} file. Only YAML files are supported."
+			var filename = activeEditor.document.fileName
+
+			console.log(`Filename is :${filename}`);
+
+			// Convert file path \\ characters to /
+			var filenameForwardSlash = filename.split('\\').join('/')
+			console.log(`Amended filename is :${filenameForwardSlash}`);
+
+			vscode.window.showInformationMessage(`Creating Env from ${filenameForwardSlash}\n This may take up to a minute...`);
+			console.log(`Creating Env from ${filenameForwardSlash}\n This may take up to a minute...`)
+
+			// Run the conda create environment command
+			const terminal_output = execSync(`conda env create -f ${filenameForwardSlash}`, { encoding: 'utf-8' });
+			console.log(`Creating env from file output:\n${terminal_output}`);
+
+			// TODO: Show progress of creating env
+			// https://stackoverflow.com/questions/43695200/how-to-implement-a-busy-indicator-in-vscode
+
+			//TODO: Get name of created environment and show to user
+			vscode.window.showInformationMessage(`Conda environment created!`);
+
+		}
+	);
+	context.subscriptions.push(buildCondaYAMLFunct);
+
+
+	/**
+	 * Shows an input box using window.showInputBox().
+	 * Higher level wrapper around vscode.window.showInputBox
+	 * Source: https://stackoverflow.com/questions/55854519/how-to-ask-user-for-username-or-other-data-with-vs-code-extension-api
+	 */
+	async function deleteInputBox() {
+		const result = await vscode.window.showInputBox({
+			value: 'example_conda_env',
+			placeHolder: 'Conda Environment Name to Delete',
+			validateInput: text => {
+				// TODO: Before this validation is called make another function that puts all conda env names into an array.
+				// Then the validation here would be to make sure the input is an element of the array.
+				vscode.window.showInformationMessage(`Validating: ${text}`);
+				return text === '123' ? 'Not 123!' : null;
+			}
+		});
+		vscode.window.showInformationMessage(`Got: ${result}`);
+
+		console.log("Running asynchronous deleteInputBox function ");
+
+		//TODO: Maybe check the string isn't null before hand.
+		//TODO: Check theres no spaces / find a regex to validate valid env names
+		//TODO: Check environment isn't currently active
+		// Delete env
+
+		// Run the conda create environment command
+		// TODO: Add loading animation so users knows extension is still running.
+		vscode.window.showInformationMessage(`Deleting Env:\n'${result}'\n This may take up to a minute...`);
+		console.log(`Deleting Env:\n'${result}'\n This may take up to a minute...`)
+		const terminal_output = execSync(`conda env remove --name ${result}`, { encoding: 'utf-8' });
+		console.log(`Delete Env terminal output:\n${terminal_output}`);
+
+		// If no errors / after try catch block
+		// TODO: Verify that env no longer shows up in updated env list "conda info --envs"
+		//	Or will more likely be using my own custom function for it
+		vscode.window.showInformationMessage(`Conda environment ${result} deleted.`);
+	}
+
+
+	// Delete Conda Env Command
+	// This command will delete a conda environment given the name of the environment from the user.
+	// Deletes environments as stated in the conda docs https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#removing-an-environment
+	let deleteCondaEnvFunct = vscode.commands.registerCommand('eggtension.deleteCondaEnv',
+		function () {
+
+			// TODO: Can I get the same list of python interpreters as used by the python extension?
+			//	Then I could filter them down to only conda envs and will be more robust and prevent me from reinventing the wheel.
+			// TODO list conda envs to use as dropdown / selection window
+			// Run conda env list and parse results into a useable object / dict
+
+
+			var response = deleteInputBox("Test Input box");
+			vscode.window.showInformationMessage(response);
+
+			console.log(response);
+
+			// TODO get response from user as to which env they want to delete
+
+			// Delete that specific env (maybe deactivate first if that is the active env)
+
+			console.log(
+				`While the deleteCondaEnvFunction has finished running.
+		The deleteInputBox function is still running in the background.`
+			);
+
+		}
+	);
+	context.subscriptions.push(deleteCondaEnvFunct);
+
+
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
-
 module.exports = {
-	activate,
-	deactivate
+	activate
 }
