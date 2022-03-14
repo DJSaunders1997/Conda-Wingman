39825c66f66d2c8fcfeadcc64af477e5c62efcd8
David Saunders <Djsaunders1997@gmail.com>
2022-03-14 23:33:02 +0000

    Activate environment (#7)
    * Initial code to parse yaml
    
    * Adding js-yaml to package lists
    
    * Move activation from yaml code to function
    
    * Adding seperate activating command
    
    * Rename icon to createEnvIcon
    
    * Created icon for activating environment
    
    * Moving status bar items to own custom class
    
    * Move listener to fix bug

--- extension.js
+++ extension.js
@@ -3,6 +3,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 const vscode = require('vscode');
 
+// Required modules to read and parse yaml
+// https://www.npmjs.com/package/js-yaml
+const yaml = require('js-yaml');
+const fs   = require('fs');
+
 // Terminal Functions copied from microsoft example terminal API
 // https://github.com/microsoft/vscode-extension-samples/blob/main/terminal-sample/src/extension.ts
 // Examples are given in TypeScript, so converted to JavaScript with an online converter https://extendsclass.com/typescript-to-javascript.html
@@ -43,43 +48,133 @@ function activeFileIsYAML() {
 	}
 }
 
-// Deactivate extension if active window changes from a YAML file.
-var listener = function (event) {
-	console.log('Active window changed', event)
 
-	// If file is not yaml then deactivate the extension
-	if ( activeFileIsYAML() ){
-		activate()
+/**
+ * 
+ * @returns the filepath of the open document
+ * 
+ * Reads the current open document, and converts the path into a more friendly format.
+ * TODO: Find out what happens if there is no open document
+ * TODO: make this OS agnostic, as I'm sure this only effects windows atm.
+ */
+function getOpenDocumentPath() {
+	var activeEditor = vscode.window.activeTextEditor;
+	var filename = activeEditor.document.fileName
 
+	console.log(`Filename is :${filename}`);
+
+	// Convert file path \\ characters to /
+	var filenameForwardSlash = filename.split('\\').join('/')
+	console.log(`Amended filename is :${filenameForwardSlash}`);
+
+	return filenameForwardSlash
+}
+
+/**
+ * 
+ * @param {string} filenameForwardSlash : filename or path to yaml environment file.
+ * 
+ * Function will read the specified yaml file and pick out the "name" value.
+ * Then attempts to activate this environment with the terminal.
+ */
+function activateEnvFromYAML(filenameForwardSlash) {
+	// Send to terminal the command to activate the environment too
+	// We can get the name by reading the YAML's value to the name: key using js-yaml
+	try {
+		const yamlDoc = yaml.load(fs.readFileSync(filenameForwardSlash, 'utf8'));
+		console.log(yamlDoc);
+
+		var env_name = yamlDoc["name"]
+
+		vscode.window.showInformationMessage(`Activating ${env_name} .`);
+		console.log(`Activating ${env_name} .`)
+
+		// Run the conda create environment command
+		var command = `conda activate ${env_name}`
+		sendCommandToTerminal(command)
+
+	} catch (e) {
+		console.log("Error parsing the yaml")
+		console.log(e);
 	}
-	else {
-		// TODO: Check if this does anything
-		console.log('Deactivate extension as YAML file not in focus')
 
-		icon.hide()
-		deactivate()
+}
+
+
+/**
+ * Class to extend the vscode createStatusBarItem with additional functionality.
+ * Represents the status bar that allows users to easily create environments.
+ * Choose symbols from this list https://code.visualstudio.com/api/references/icons-in-labels#icon-listing
+ */
+ class customStatusBarItem{
+	constructor(defaultText, tooltip, command){
+		this.defaultText = defaultText
+
+		this.statusBar = vscode.window.createStatusBarItem() // createStatusBarItem('createEnvStatusBar',1)
+		this.statusBar.text = defaultText
+		this.statusBar.tooltip = tooltip
+		this.statusBar.command = command
+		
+		this.displayDefault()
 	}
 
-};
+	/***
+	 * Returning text to default state.
+	 */
+	displayDefault() {
+		this.statusBar.text = this.defaultText
 
-var subscription = vscode.window.onDidChangeActiveTextEditor(listener);
-//subscription.dispose(); // stop listening for more active file changes
+		if (activeFileIsYAML) {
+			this.statusBar.show()
+		}
+	}
+	/**
+	 * To be displayed when action is running from the button being selected.
+	 * 
+	 * TODO: Loading functionality doesn't work find a fix.
+	 */
+	displayLoading() {
+		this.statusBar.text = this.defaultText + ' $(loading~spin)'
 
-// Create Status Bar Icon
-// Select Icons from this list https://code.visualstudio.com/api/references/icons-in-labels#icon-listing
-var icon = vscode.window.createStatusBarItem('createEnvStatusBar',1)
-icon.text = '$(flame)Create Env from YAML $(note)'
-icon.color = 'Gold'
-icon.tooltip = 'Click here to create a conda environment from the open YAML file'
-icon.command = 'conda-wingman.buildCondaYAML'
+		if (activeFileIsYAML) {
+			this.statusBar.show()
+		}
+	}
 
+}
+
+
+/**
+ * Function that is run on activation of extension.
+ * Here the main functionality of the function is defined.
+ * 
+ */
 function activate(context) {
 
 	console.log('Congratulations, your extension "Conda Wingman" is now active!');
 
-	icon.show() //TODO check file is yaml file
 
-	// Build Conda Env Command
+	var createEnvIcon 	= new customStatusBarItem('$(tools) Build Env from YAML', 'Build conda environment from open YAML file', 'conda-wingman.buildCondaYAML')
+	var activateEnvIcon = new customStatusBarItem('$(symbol-event) Activate Env from YAML', 'Activate conda environment referenced in open YAML file', 'conda-wingman.activateCondaYAML')
+
+	// Setup listener to see when active file is not YAML
+	var listener = function (event) {
+		console.log('Active window changed', event)
+
+		// If file is not yaml then deactivate the extension
+		if ( !activeFileIsYAML() ){
+			createEnvIcon.hide()
+			activateEnvIcon.hide()
+		}
+
+	};
+
+	var fileChangeSubscription = vscode.window.onDidChangeActiveTextEditor(listener);
+	//subscription.dispose(); // stop listening for more active file changes
+
+
+
+	// Command: "Conda Wingman: Build Conda Environment from YAML file"
 	// This command will build a conda environment from the file active in window.
 	let buildCondaYAMLFunct = vscode.commands.registerCommand('conda-wingman.buildCondaYAML',
 		function () {
@@ -88,23 +183,27 @@ function activate(context) {
 
 			// Validate open file is YAML
 			if( activeFileIsYAML() ) {
-				var activeEditor = vscode.window.activeTextEditor;
+				
+				var filenameForwardSlash = getOpenDocumentPath()
 
-				var filename = activeEditor.document.fileName
-
-				console.log(`Filename is :${filename}`);
-
-				// Convert file path \\ characters to /
-				var filenameForwardSlash = filename.split('\\').join('/')
-				console.log(`Amended filename is :${filenameForwardSlash}`);
-
+				// TODO: add $(loading~spin) onto status bar until after terminal has finished running command 
 				vscode.window.showInformationMessage(`Creating Env from ${filenameForwardSlash}\n This may take up to a minute...`);
 				console.log(`Creating Env from ${filenameForwardSlash}\n This may take up to a minute...`)
+				
+				//Add loading icon to bar
+				createEnvIcon.displayLoading()
+
+				console.log("Display the loading bar")
 
 				// Run the conda create environment command
 				var command = `conda env create -f "${filenameForwardSlash}"`
 				sendCommandToTerminal(command)
 
+				activateEnvFromYAML(filenameForwardSlash)
+
+				// Remove loading icon from bar
+				createEnvIcon.displayDefault()
+				console.log("Now remove the loading bar")
 			}
 			else {
 				// split string by . and return last array element to get extension
@@ -115,6 +214,33 @@ function activate(context) {
 		}
 	);
 	context.subscriptions.push(buildCondaYAMLFunct);
+
+	// Command: "Conda Wingman: Activate Conda Environment from YAML file"
+	// This command will activate the Conda environment named in the opened YAML file.
+	let activateCondaYAMLFunct = vscode.commands.registerCommand('conda-wingman.activateCondaYAML',
+	function () {
+
+		var activeFilename = vscode.window.activeTextEditor.document.fileName
+
+		// Validate open file is YAML
+		if( activeFileIsYAML() ) {
+			var filenameForwardSlash = getOpenDocumentPath()
+
+			activateEnvIcon.displayLoading()
+			activateEnvFromYAML(filenameForwardSlash)
+
+			//Remove loading icon from bar
+			activateEnvIcon.displayDefault()
+		}
+		else {
+			// split string by . and return last array element to get extension
+			var fileExt = activeFilename.split('.').pop();
+			vscode.window.showErrorMessage(`Cannot read conda env info from a ${fileExt} file. Only YAML files are supported.`);
+		}
+
+	}
+);
+context.subscriptions.push(activateCondaYAMLFunct);
 
 	// Command: "Conda Wingman: Update open YAML file from the active Conda Environment"
 	// This command will update the open requirements.yaml with packages from the active environment.
@@ -208,7 +334,7 @@ function activate(context) {
 
 // this method is called when your extension is deactivated
 function deactivate() {
-	icon.hide()
+	//fileChangeSubscription.dispose(); // stop listening for more active file changes
 }
 
 module.exports = {
